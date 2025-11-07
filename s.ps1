cfb6570fad948cbfc3fea3b3a1b0d5e6d58cea1a
Ollie <oliverfawkesg@gmail.com>
2025-11-07 11:38:54 +0000

    last ignores

--- s.ps1
+++
@@ -1,3 +0,0 @@
-Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd backend\Drone-Project\Drone-Project; .\mvnw.cmd spring-boot:run'
-Start-Sleep -Seconds 5
-Start-Process powershell -ArgumentList '-NoExit', '-Command', 'cd frontend\dfront; npm start'
